d67d744dcb3a9e6f7bd74847decfe0cd7a832829
padri <muhammad.padri@weekendinc.com>
2021-08-03 19:20:49 +0700

    slicing ui

--- src/screen/BarangKeluar.js
+++ src/screen/BarangKeluar.js
@@ -1,11 +1,59 @@
-import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import React, {useState} from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  TouchableOpacity,
+} from 'react-native';
 import Header from '../components/Header';
 
-const BarangKeluar = () => {
+const BarangKeluar = ({navigation}) => {
+  const [kode, setKode] = useState('');
+  const [nama, setNama] = useState('');
+  const [value, setValue] = useState(0);
+
+  const plus = () => {
+    let newValue = value + 1;
+    setValue(newValue);
+  };
+  const minus = () => {
+    let newValue = value - 1;
+    if (newValue >= 0) {
+      setValue(newValue);
+    }
+  };
+
   return (
     <View style={styles.container}>
       <Header title="barang-keluar" />
+      <View style={styles.wrapInput}>
+        <TextInput
+          value={kode}
+          onChangeText={val => setKode(val)}
+          placeholder="Kode Barang"
+          style={styles.input}
+        />
+        <TextInput
+          value={nama}
+          placeholder="Nama Barang"
+          style={styles.input}
+        />
+      </View>
+      <View style={styles.wrap}>
+        <View style={styles.wrapCount}>
+          <TouchableOpacity style={styles.wrapBtnCount} onPress={() => plus()}>
+            <Text style={styles.icon}>+</Text>
+          </TouchableOpacity>
+          <Text style={styles.count}>{value}</Text>
+          <TouchableOpacity style={styles.wrapBtnCount} onPress={() => minus()}>
+            <Text style={styles.icon}>-</Text>
+          </TouchableOpacity>
+        </View>
+        <TouchableOpacity style={styles.btn}>
+          <Text style={styles.textBtn}>Simpan</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
@@ -16,5 +64,51 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 16,
     backgroundColor: '#fff',
+  },
+  wrapInput: {
+    marginVertical: 24,
+  },
+  input: {
+    marginVertical: 8,
+    padding: 8,
+    borderWidth: 1,
+    borderColor: '#020202',
+    borderRadius: 8,
+  },
+  wrap: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  wrapCount: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  count: {
+    fontSize: 24,
+    marginHorizontal: 24,
+    fontWeight: 'bold',
+  },
+  icon: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  wrapBtnCount: {
+    backgroundColor: '#fff',
+    paddingHorizontal: 16,
+    paddingVertical: 6,
+    borderRadius: 100,
+    elevation: 4,
+  },
+  btn: {
+    width: 89,
+    alignItems: 'center',
+    paddingVertical: 16,
+    borderRadius: 8,
+    elevation: 4,
+    backgroundColor: '#57F31F',
+  },
+  textBtn: {
+    textAlign: 'center',
   },
 });
--- src/screen/EditBarang.js
+++ src/screen/EditBarang.js
@@ -1,12 +1,80 @@
-import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 import Header from '../components/Header';
 
-const EditBarang = () => {
+const EditBarang = ({navigation}) => {
+  const [kode, setKode] = useState('');
+  const [nama, setNama] = useState('');
+  const [qty, setQty] = useState('');
+  const [ket, setKet] = useState('');
+
+  const goBack = () => navigation.goBack();
+
+  const onSubmit = () => {
+    const data = {
+      kode,
+      nama,
+      qty,
+      ket,
+    };
+    if (!kode && !nama && !qty && !ket) {
+      alert('Harap Lengkapi Data');
+    } else {
+      console.log(data);
+    }
+  };
+
   return (
-    <View style={styles.container}>
+    <ScrollView style={styles.container}>
       <Header title="edit" />
-    </View>
+      <View style={styles.wrapInput}>
+        <TextInput
+          value={kode}
+          onChangeText={val => setKode(val)}
+          placeholder="Kode Barang"
+          style={styles.input}
+        />
+        <TextInput
+          value={nama}
+          onChangeText={val => setNama(val)}
+          placeholder="Nama Barang"
+          style={styles.input}
+        />
+        <TextInput
+          value={qty}
+          onChangeText={val => setQty(val)}
+          placeholder="Qty"
+          style={styles.input}
+          keyboardType="number-pad"
+        />
+        <TextInput
+          value={ket}
+          onChangeText={val => setKet(val)}
+          placeholder="Keterangan"
+          style={styles.input}
+        />
+      </View>
+      <View style={styles.wrapBtn}>
+        <TouchableOpacity
+          style={[styles.btn, {backgroundColor: '#DD404A'}]}
+          onPress={() => goBack()}>
+          <Text style={styles.textBtn}>Kembali</Text>
+        </TouchableOpacity>
+        <View style={{width: 32}} />
+        <TouchableOpacity
+          style={[styles.btn, {backgroundColor: '#57F31F'}]}
+          onPress={() => onSubmit()}>
+          <Text style={styles.textBtn}>Simpan</Text>
+        </TouchableOpacity>
+      </View>
+    </ScrollView>
   );
 };
 
@@ -16,5 +84,30 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 16,
     backgroundColor: '#fff',
+  },
+  wrapInput: {
+    marginVertical: 24,
+  },
+  input: {
+    marginVertical: 8,
+    padding: 8,
+    borderWidth: 1,
+    borderColor: '#020202',
+    borderRadius: 8,
+  },
+  wrapBtn: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  btn: {
+    width: 89,
+    alignItems: 'center',
+    paddingVertical: 16,
+    borderRadius: 8,
+    elevation: 4,
+  },
+  textBtn: {
+    textAlign: 'center',
   },
 });
